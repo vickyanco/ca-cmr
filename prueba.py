f478877e3ffa3f59317130b51630ef72eea978a1
vickyanco <vickyanco@gmail.com>
2024-07-17 20:19:04 -0300

    Read a folder, wrong path

--- prueba.py
+++ prueba.py
@@ -1,14 +1,27 @@
 import pydicom
 import matplotlib.pyplot as plt
-import numpy as np
 import os
 
-# DICOM folder path
-dicom_folder_path = 'Users/USUARIO/Desktop/51'
+# Define the folder path containing the DICOM files
+folder_path = 'C:/Users/USUARIO/Desktop/51'
 
-# read the DICOM file
-dicom = pydicom.dcmread(dicom_folder_path)
-for filename in os.listdir(dicom_folder_path):
-    if filename.endswith('.dcm'):  
-        file_path = os.path.join(dicom_folder_path, filename)
-        dicom_data = pydicom.dcmread(file_path)
+# List to store the DICOM datasets
+dicom_datasets = []
+        
+# Iterate over all files in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith(".dcm"):  # Check if the file is a DICOM file
+        file_path = os.path.join(folder_path, filename)
+        # Read the DICOM file
+        dicom_dataset = pydicom.dcmread(file_path)
+
+        # Add it to the list
+        dicom_datasets.append(dicom_dataset)
+        
+        # Get the pixel data (image data)
+        image_data = dicom_dataset.pixel_array
+        
+        # Display the image
+        plt.imshow(image_data, cmap=plt.cm.gray)
+        plt.title(f"File: {filename}")
+        plt.show()
