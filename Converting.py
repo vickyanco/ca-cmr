1a1103e962fc836fb531870da834df144cbeaeaa
vickyanco <maria.anconetani@hospitalitaliano.org.ar>
2024-09-23 20:59:22 -0300

    Convertion to png

---
+++ Converting.py
@@ -0,0 +1,25 @@
+# file: Converting.py
+# description: Converting dicom to png.
+# author: María Victoria Anconetani
+# date: 23/09/2024
+
+import cv2
+import os
+import pydicom
+
+inputdir = 'D:/CA EN CMR/raw'
+outdir = 'D:/CA EN CMR/convertidas'
+
+for root, dirs, files in os.walk(inputdir):
+    relative_path = os.path.relpath(root, inputdir)
+    output_dir = os.path.join(outdir, relative_path)
+    os.makedirs(output_dir, exist_ok=True)
+    
+    for file in files:
+        if file.endswith('.dcm'):  
+            file_path = os.path.join(root, file) 
+            ds = pydicom.dcmread(file_path)  
+            img = ds.pixel_array  
+            output_file = os.path.join(output_dir, file.replace('.dcm', '.png')) 
+            cv2.imwrite(output_file, img)
+            print(f'Guardado: {output_file}')
